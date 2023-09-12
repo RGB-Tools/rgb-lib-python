d28bb35b86ab496f32d053fc325048cdba696dda
Nicola Busanello <nicola.busanello@gmail.com>
2023-09-12 18:03:21 +0200

    update Dockerfile to rust 1.72 + build in debug mode

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rust:1.71-slim-bullseye
+FROM rust:1.72-slim-bullseye
 
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
--- generate.sh
+++ generate.sh
@@ -9,10 +9,10 @@ RGBLIBFFI_PATH="./rgb-lib/rgb-lib-ffi"
 MANIFEST_PATH=(--manifest-path "$RGBLIBFFI_PATH/Cargo.toml")
 
 echo "Generating librgblibffi.so..."
-cargo build --release "${MANIFEST_PATH[@]}"
-cp "$RGBLIBFFI_PATH/target/release/librgblibffi.so" "$PY_SRC/"
+cargo build "${MANIFEST_PATH[@]}"
+cp "$RGBLIBFFI_PATH/target/debug/librgblibffi.so" "$PY_SRC/"
 
 echo "Generating rgb_lib.py..."
-cargo run --release "${MANIFEST_PATH[@]}" \
+cargo run "${MANIFEST_PATH[@]}" \
     --bin rgb-lib-ffi-bindgen generate $RGBLIBFFI_PATH/src/rgb-lib.udl \
-    --out-dir $PY_SRC --language python --no-format
+    --out-dir "$PY_SRC" --language python --no-format
